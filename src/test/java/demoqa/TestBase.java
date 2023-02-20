fdb9c1f81aa4d5813c053582f9e58efe0330898f
Islam-Kaziev <kaziev19691@gmail.com>
2023-02-20 21:22:22 +0300

    added demoqa test

---
+++ src/test/java/demoqa/TestBase.java
@@ -0,0 +1,33 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        // screenshot
+        // page source
+        // console logs
+        // video
+    }
+}
